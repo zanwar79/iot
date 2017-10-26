cf8bb4457b8f09751ea8104980011c4f9ec36504
zanwar79 <zahidsbusinessaccount@gmail.com>
2017-10-26 15:51:55 -0500

    Update twoswitches.py

--- twoswitches.py
+++ twoswitches.py
@@ -4,13 +4,13 @@ import time
 
 # setup pins
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3, GPIO.IN)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(5, GPIO.OUT)
-GPIO.setup(7, GPIO.IN)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # while loop to constantly check pins
 while True:
-    if (GPIO.input(3) and GPIO.input(7) ) == GPIO.HIGH:
+    if (GPIO.input(8) and GPIO.input(10) ) == GPIO.HIGH:
     # flash output pin 5
         GPIO.output(5, GPIO.HIGH)
 
